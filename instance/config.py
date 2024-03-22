0035ef9e4f3dc78be16df1b97bb5a28e5941b7f7
irul11 <amtaqiy11@gmail.com>
2024-03-22 12:16:39 +0700

    fix config and load env

--- instance/config.py
+++ instance/config.py
@@ -1,14 +1,3 @@
 DEBUG = True
-SECRET_KEY = 'Inirahasiabanget'
-STRIPE_API_KEY = 'Inirahasia'
-
-DB_NAME = 'postgres'
-DB_PORT = 5432
-DB_USER = 'postgres.mzenkcoirmljfncenusg'
-DB_HOST = 'aws-0-ap-southeast-1.pooler.supabase.com'
-DB_PASSWORD = 'm4rZcgNKkk1aqBGE'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.mzenkcoirmljfncenusg:m4rZcgNKkk1aqBGE@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 SQLALCHEMY_TRACK_MODIFICATION = False
-
-MAIL_PASSWORD = 'ewkjuxrvqzhbudkz'
 MAIL_USERNAME = 'tsukkisan102@gmail.com'
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from mainapp import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
